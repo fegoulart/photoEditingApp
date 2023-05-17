adb326ebf4b50b2057a82a68a367171843c74368
Fernando Luiz Goulart <fgoulart.exactaworks@alelo.com.br>
2023-05-17 12:14:46 -0300

    first commit

---
+++ PhotoEditingApp/SceneDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        window = UIWindow()
+        window?.windowScene = scene as? UIWindowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ PhotoEditingApp/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+}
+
