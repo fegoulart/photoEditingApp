cf6d3ea5320bc711d1279decf1b14470d8a40a85
Fernando Luiz Goulart <fgoulart.exactaworks@alelo.com.br>
2023-05-18 19:58:47 -0300

    SceneDelegate as the composition root

--- PhotoEditingApp/SceneDelegate.swift
+++ PhotoEditingApp/SceneDelegate.swift
@@ -5,9 +5,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        let imagePicker = PhotoEditingImagePicker(pickerController: UIImagePickerController())
+        let startAction = UIAction { (action) in
+            imagePicker.present()
+        }
+        let photoEditingView = PhotoEditingView(startAction: startAction)
+
+        photoEditingView.startAction = startAction
+        let viewModel = ViewModel(cacheImageService: FileCacheImageService())
+        let viewController = ViewController(view: photoEditingView, imagePicker: imagePicker, viewModel: viewModel)
+        imagePicker.delegate = viewController
+        let navigationController = UINavigationController(rootViewController: viewController)
+        imagePicker.presentationController = viewController
+
         window = UIWindow()
         window?.windowScene = scene as? UIWindowScene
-        window?.rootViewController = ViewController(view: PhotoEditingView())
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
