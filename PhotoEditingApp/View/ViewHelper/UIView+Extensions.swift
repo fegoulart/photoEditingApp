f52104d8492b6d7348f68dbb0f830ff1aacca4a4
Fernando Luiz Goulart <fgoulart.exactaworks@alelo.com.br>
2023-05-20 12:55:47 -0300

    UIView fadeIn and fadeOut helpers

--- PhotoEditingApp/View/ViewHelper/UIView+Extensions.swift
+++ PhotoEditingApp/View/ViewHelper/UIView+Extensions.swift
@@ -24,4 +24,30 @@ extension UIView {
     func showPreview() -> some View {
         Preview(view: self)
     }
+
+
+    func fadeIn(_ duration: TimeInterval = 0.2, onCompletion: (() -> Void)? = nil) {
+        assert(Thread.isMainThread)
+        self.alpha = 0
+        self.isHidden = false
+        UIView.animate(
+            withDuration: duration,
+            animations: { self.alpha = 1 },
+            completion: { (value: Bool) in
+                if let complete = onCompletion { complete() }
+            }
+        )
+    }
+
+    func fadeOut(_ duration: TimeInterval = 0.2, onCompletion: (() -> Void)? = nil) {
+        assert(Thread.isMainThread)
+        UIView.animate(
+            withDuration: duration,
+            animations: { self.alpha = 0 },
+            completion: { (value: Bool) in
+                self.isHidden = true
+                if let complete = onCompletion { complete() }
+            }
+        )
+    }
 }
