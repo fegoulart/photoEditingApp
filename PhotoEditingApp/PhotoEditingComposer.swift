ec2feca865456dc97788119f55d7a1e19249fa77
Fernando Luiz Goulart <fgoulart.exactaworks@alelo.com.br>
2023-05-21 19:45:40 -0300

    refactor: composition root with PhotoEditingComposer class

---
+++ PhotoEditingApp/PhotoEditingComposer.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+struct PhotoEditingComposer {
+    static func makeViewController() -> ViewController {
+        let permissionChecker = PhotoEditingAppPermissionChecker()
+        let imagePickerViewModel = ImagePickerViewModel(permissionChecker: permissionChecker)
+        let imagePicker = PhotoEditingImagePicker(pickerController: UIImagePickerController(), pickerViewModel: imagePickerViewModel)
+        let startAction: PhotoEditingView.ButtonHandler = { [weak imagePicker] in
+            imagePicker?.present()
+        }
+
+        let viewModel = ViewModel(cacheImageService: FileCacheImageService())
+        let deleteAction: PhotoEditingView.ButtonHandler = { [weak viewModel] in
+            viewModel?.clear()
+        }
+
+        let imageSaver = PhotoAlbumSaver()
+        let saveAction: (UIImage?) -> Void = { [weak viewModel] image in
+            imageSaver.writeToPhotoAlbum(image: image) { result in
+                switch result {
+                case .success:
+                    viewModel?.setOperationResult("Image successfully saved")
+                    viewModel?.clear()
+                case .failure:
+                    viewModel?.setOperationResult("Error: please try again later")
+                }
+            }
+        }
+        let photoEditingView = PhotoEditingView(startAction: startAction, deleteAction: deleteAction, saveAction: saveAction)
+
+        photoEditingView.startAction = startAction
+        let viewController = ViewController(view: photoEditingView, imagePicker: imagePicker, viewModel: viewModel, stateChangeManager: ViewStateChangeManager())
+        imagePicker.delegate = viewController
+        imagePicker.presentationController = viewController
+        return viewController
+    }
+}
--- PhotoEditingApp/SceneDelegate.swift
+++ PhotoEditingApp/SceneDelegate.swift
@@ -6,37 +6,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-        let permissionChecker = PhotoEditingAppPermissionChecker()
-        let imagePickerViewModel = ImagePickerViewModel(permissionChecker: permissionChecker)
-        let imagePicker = PhotoEditingImagePicker(pickerController: UIImagePickerController(), viewModel: imagePickerViewModel)
-        let startAction: PhotoEditingView.ButtonHandler = { [weak imagePicker] in
-            imagePicker?.present()
-        }
-
-        let viewModel = ViewModel(cacheImageService: FileCacheImageService())
-        let deleteAction: PhotoEditingView.ButtonHandler = { [weak viewModel] in
-            viewModel?.clear()
-        }
-
-        let imageSaver = PhotoAlbumSaver()
-        let saveAction: (UIImage?) -> Void = { [weak viewModel] image in
-            imageSaver.writeToPhotoAlbum(image: image) { result in
-                switch result {
-                case .success:
-                    viewModel?.setOperationResult("Image successfully saved")
-                    viewModel?.clear()
-                case .failure:
-                    viewModel?.setOperationResult("Error: please try again later")
-                }
-            }
-        }
-        let photoEditingView = PhotoEditingView(startAction: startAction, deleteAction: deleteAction, saveAction: saveAction)
-
-        photoEditingView.startAction = startAction
-        let viewController = ViewController(view: photoEditingView, imagePicker: imagePicker, viewModel: viewModel, stateChangeManager: ViewStateChangeManager())
-        imagePicker.delegate = viewController
-        let navigationController = UINavigationController(rootViewController: viewController)
-        imagePicker.presentationController = viewController
+        let navigationController = UINavigationController(rootViewController: PhotoEditingComposer.makeViewController())
 
         window = UIWindow()
         window?.windowScene = scene as? UIWindowScene
