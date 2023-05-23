e3e05097d1aa1f714487b35b7b59a7df4912543f
Fernando Luiz Goulart <fgoulart.exactaworks@alelo.com.br>
2023-05-23 18:46:23 -0300

    fix: uicollectionview layout

--- PhotoEditingApp/View/PhotoEditingView/PhotoEditingView+CollectionView.swift
+++ PhotoEditingApp/View/PhotoEditingView/PhotoEditingView+CollectionView.swift
@@ -10,15 +10,15 @@ extension PhotoEditingView {
             let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
                                                   heightDimension: .fractionalHeight(1))
 
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33),
                                                    heightDimension: .fractionalHeight(1))
 
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
                                                            subitem: item,
-                                                           count: 2)
-            group.interItemSpacing = .fixed(16)
+                                                           count: 1)
+            group.interItemSpacing = .fixed(8)
 
             let section = NSCollectionLayoutSection(group: group)
 
